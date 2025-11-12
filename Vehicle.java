fb22a857fbd57d3093669b1a067e2ac61ec3ad1d
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-12 12:31:38 +0200

    Am adaugat constructorul fara parametri al clasei principale Vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -13,6 +13,21 @@ abstract public class Vehicle {
     private int kmParcursi;
     private int nrLocuri;
 
-    
+    public Vehicle()
+    {
+        this.nrRoti = 0;
+        this.culoare = "Rosu";
+        this.brand = "Toyota";
+        this.anLansare = 2009;
+        this.pret = 0;
+        this.tipCombustibil = "Benzina";
+        this.putereMotor = 0;
+        this.tipCutieViteze = "Manuala";
+        this.inaltime = 0;
+        this.capRezervor = 0;
+        this.vitMaxima = 0;
+        this.kmParcursi = 0;
+        this.nrLocuri = 0;
+    }
 
 }
