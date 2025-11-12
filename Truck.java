0d7195a04c8c3d416e0b64f9879c077a673ad3d7
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-13 00:08:43 +0200

    adaugat constructorul fara parametri in clasa parinte Truck

--- Truck.java
+++ Truck.java
@@ -1,4 +1,14 @@
 public class Truck extends Vehicle{
     private int capPortbagaj;
     private String tipCaroserie;
+
+    public Truck()
+    {
+        super();
+        this.capPortbagaj = 0;
+        this.tipCaroserie = "Frigorific";
+    }
+
+
+
 }
