da6b4e02203d7d235cae9dc0729ee516cafc2099
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-13 00:10:02 +0200

    adaugat constructor cu toti parametrii in clasa parinte Truck

--- Truck.java
+++ Truck.java
@@ -9,6 +9,14 @@ public class Truck extends Vehicle{
         this.tipCaroserie = "Frigorific";
     }
 
+    public Truck(int nrRoti, String culoare, String brand, int anLansare, int pret, String tipCombustibil, int putereMotor, String tipCutieViteze, int inaltime, int capRezervor, int vitMaxima, int kmParcursi, int nrLocuri,
+    int capPortbagaj, String tipCaroserie)
+    {
+        super(nrRoti, culoare, brand, anLansare, pret, tipCombustibil, putereMotor, tipCutieViteze, inaltime, capRezervor, vitMaxima, kmParcursi, nrLocuri);
+        this.capPortbagaj = capPortbagaj;
+        this.tipCaroserie = tipCaroserie;
+    }
 
 
+    
 }
