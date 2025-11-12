a8e72d0705cade53cfd3637302943611c9116d5b
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-12 13:29:15 +0200

    - am adaugat 2 clase noi derivate din Vehicle: Car si Truck - am adaugat datele membre private de instanta specifice clasei Car: accecl0100 (int), capPortbagaj (int), tipCaroserie (String) - am adaugat constructorul fara parametri al clasei Car

---
+++ Car.java
@@ -0,0 +1,16 @@
+public class Car extends Vehicle{
+
+    private int accel0100;
+    private int capPortbagaj;
+    private String tipCaroserie;
+
+    public Car()
+    {
+        super();
+        this.accel0100 = 0;
+        this.capPortbagaj = 0;
+        this.tipCaroserie = "Hitchback";
+    }
+
+    
+}
