de2707e7c93fdaa4561e0b74d92c34aab27034e3
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-12 12:23:26 +0200

    am adaugat datele membre ale clasei principale Vehicle si anume: - nrRoti - culoare - brand - anLansare - pret - tipCombustibil - putereMotor - tipCutieViteze - inaltime - capRezervor - vitMaxima - kmParcursi - nrLocuri

--- Vehicle.java
+++ Vehicle.java
@@ -1,6 +1,18 @@
 abstract public class Vehicle {
+    private int nrRoti;
+    private String culoare;
+    private String brand;
+    private int anLansare;
+    private int pret;
+    private String tipCombustibil;
+    private int putereMotor;
+    private String tipCutieViteze;
+    private int inaltime;
+    private int capRezervor;
+    private int vitMaxima;
+    private int kmParcursi;
+    private int nrLocuri;
 
     
 
-    
 }
