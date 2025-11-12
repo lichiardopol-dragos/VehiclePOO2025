68d8759ecc9953c6819a0973d669b15164dfb518
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-12 13:32:12 +0200

    am adaugat constructorul cu toi parametrii al clasei copil Car

--- Car.java
+++ Car.java
@@ -12,5 +12,12 @@ public class Car extends Vehicle{
         this.tipCaroserie = "Hitchback";
     }
 
-    
+    public Car(int nrRoti, String culoare, String brand, int anLansare, int pret, String tipCombustibil, int putereMotor, String tipCutieViteze, int inaltime, int capRezervor, int vitMaxima, int kmParcursi, int nrLocuri,
+    int accel0100, int capPortbagaj, String tipCaroserie)
+    {
+        super(nrRoti, culoare, brand, anLansare, pret, tipCombustibil, putereMotor, tipCutieViteze, inaltime, capRezervor, vitMaxima, kmParcursi, nrLocuri);
+        this.accel0100 = accel0100;
+        this.capPortbagaj = capPortbagaj;
+        this.tipCaroserie = tipCaroserie;
+    }
 }
