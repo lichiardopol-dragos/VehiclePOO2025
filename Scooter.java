75e758ed2a551699e696c1939c5cc7ac78ea50e7
chiri <chiriloaie.cosmin.alin@gmail.com>
2025-11-13 10:09:33 +0200

    date membre cu getter setter si tostring

---
+++ Scooter.java
@@ -0,0 +1,54 @@
+public class Scooter extends Vehicle {
+
+    //atribute private
+    private boolean hasStorageUnderSeat;
+    private int dimensiuneRoata;
+
+    //constructor fara parametri
+    public Scooter() {
+        super();
+        this.hasStorageUnderSeat = true;
+        this.dimensiuneRoata = 12;
+    }
+
+
+    //LOC PT OVERRIDE METODE
+
+
+
+    //constructor cu toti parametrii
+public Scooter(int nrRoti, String culoare, String brand, int anLansare, int pret,
+               String tipCombustibil, int putereMotor, String tipCutieViteze,
+               int inaltime, int capRezervor, int vitMaxima,
+               int kmParcursi, int nrLocuri, boolean hasStorageUnderSeat, int dimensiuneRoata) {
+    super(nrRoti, culoare, brand, anLansare, pret, tipCombustibil, putereMotor,
+            tipCutieViteze, inaltime, capRezervor, vitMaxima, kmParcursi, nrLocuri);
+    this.hasStorageUnderSeat = hasStorageUnderSeat;
+    this.dimensiuneRoata = dimensiuneRoata;
+}
+
+//constructor de copiere
+    public Scooter(Scooter b)
+    {
+        super(b);
+        this.hasStorageUnderSeat = b.hasStorageUnderSeat;
+        this.dimensiuneRoata = b.dimensiuneRoata;
+}
+
+//metoda toString
+
+
+    //gettere si settere pentru cele doua atribute private
+    public boolean isHasStorageUnderSeat(){
+        return hasStorageUnderSeat;
+    }
+    public void setHasStorageUnderSeat(boolean hasStorageUnderSeat){
+        this.hasStorageUnderSeat = hasStorageUnderSeat;
+    }
+    public int getDimensiuneRoata(){
+        return dimensiuneRoata;
+    }
+    public void setDimensiuneRoata(int dimensiuneRoata){
+        this.dimensiuneRoata = dimensiuneRoata;
+    }
+               }
