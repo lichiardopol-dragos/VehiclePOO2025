e3edf20f6e76c800ec2a64e568a4a49a6c5765ed
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-20 00:28:02 +0200

    - redenumire fisier in ParcurgereVectCarTruck - crearea vectorului specific fisierului (ptr fiecare din echipa, un vector cu cate 10 elemente din fiecare clasa)

---
+++ ParcurgereVectCarTruck.java
@@ -0,0 +1,110 @@
+import java.util.*;
+
+public class ParcurgereVectCarTruck {
+    public static void main(String[] args) {
+
+        Car c1 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+                
+        Car c2 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c3 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c4 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c5 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c6 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c7 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c8 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c9 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Car c10 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
+        100, "Manuala", 100, 20, 
+        240, 0, 5,
+        8, 10, "Hitchback");
+
+        Truck t1 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t2 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t3 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t4 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t5 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t6 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t7 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t8 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t9 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        Truck t10 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
+         100, "Manuala", 100, 50, 
+         180, 0, 1, 600, 
+         "Inchis");
+
+        //definire ArrayList
+        ArrayList<Vehicle> vehicles = new ArrayList<Vehicle>(10);
+
+    }
+}
