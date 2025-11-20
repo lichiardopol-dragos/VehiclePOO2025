7b73a1ac7e4399db3a760b94dfe66592977f3678
lichiardopol-dragos <dl203@student.ugal.ro>
2025-11-20 02:59:36 +0200

    - am inceput sa schimb variabilele fiecarei instante generate in parcurgeri pentru mai multa varietate in date

--- ParcurgereVectCarTruck.java
+++ ParcurgereVectCarTruck.java
@@ -13,98 +13,100 @@ public class ParcurgereVectCarTruck {
         240, 0, 5,
         8, 10, "Hitchback");
                 
-        Car c2 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c2 = new Car(4, "Oranj", "Hyundai", 2010, 8500, "Benzina",
+        180, "Automata", 145, 34, 
+        238, 29056, 5,
+        8, 14, "Bicorp");
 
-        Car c3 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c3 = new Car(4, "Violet", "Peugeot", 2005, 7000, "Electrica",
+        130, "Manuala", 186, 33, 
+        221, 39172, 4,
+        6, 12, "Hitchback");
 
-        Car c4 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c4 = new Car(4, "Cyan", "Toyota", 2013, 14000, "Benzina",
+        135, "Automata", 105, 32, 
+        152, 7787, 6,
+        9, 15, "Hitchback");
 
-        Car c5 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c5 = new Car(4, "Turcoaz", "Tesla", 2018, 50000, "Electrica",
+        195, "Manuala", 107, 33, 
+        162, 90102, 2,
+        10, 14, "Hitchback");
 
-        Car c6 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c6 = new Car(4, "Alb", "Renault", 2012, 18000, "Benzina",
+        183, "Manuala", 143, 24, 
+        175, 76573, 3,
+        8, 10, "Bicorp");
 
-        Car c7 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c7 = new Car(4, "Visiniu", "Chevrolet", 2003, 10000, "Benzina",
+        247, "Automata", 180, 20, 
+        235, 19408, 6,
+        8, 10, "Bicorp");
 
-        Car c8 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c8 = new Car(4, "Galben", "Honda", 2009, 3000, "Benzina",
+        134, "Manuala", 119, 27, 
+        201, 8252, 4,
+        10, 12, "Hitchback");
 
-        Car c9 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c9 = new Car(4, "Gri inchis", "Fiat", 2010, 11000, "Motorina",
+        242, "Manuala", 166, 35, 
+        211, 5453, 5,
+        10, 13, "Hitchback");
 
-        Car c10 = new Car(4, "Verde", "Dacia", 2014, 8000, "Benzina",
-        100, "Manuala", 100, 20, 
-        240, 0, 5,
-        8, 10, "Hitchback");
+        Car c10 = new Car(4, "Verde", "Kia", 2020, 44000, "Motorina",
+        231, "Automata", 152, 28, 
+        120, 12037, 4,
+        9, 15, "Bicorp");
 
-        Truck t1 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        // continua de la tipCutieViteze
+
+        Truck t1 = new Truck(6, "Gri", "DAF", 2006, 30000, "Benzina",
+         50, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t2 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t2 = new Truck(4, "Negru", "DAF", 2021, 34311, "Benzina",
+         93, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t3 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t3 = new Truck(4, "Gri", "Nissan", 2023, 32689, "Motorina",
+         130, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t4 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t4 = new Truck(4, "Rosu", "Iveco", 2008, 30648, "Motorina",
+         75, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t5 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t5 = new Truck(4, "Galben", "DAF", 2007, 28591, "Benzina",
+         120, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t6 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t6 = new Truck(6, "Albastru", "Volvo", 2016, 25840, "Benzina",
+         90, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t7 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t7 = new Truck(4, "Gri", "Nissan", 2015, 30580, "Motorina",
+         138, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t8 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t8 = new Truck(4, "Violet", "DAF", 2006, 31460, "Benzina",
+         62, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t9 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t9 = new Truck(6, "Gri", "DAF", 2025, 29270, "Motorina",
+         138, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
-        Truck t10 = new Truck(4, "Gri", "DAF", 2006, 30000, "Benzina",
-         100, "Manuala", 100, 50, 
+        Truck t10 = new Truck(6, "Alb", "Fiat", 2006, 25862, "Benzina",
+         68, "Manuala", 100, 50, 
          180, 0, 1, 600, 
          "Inchis");
 
